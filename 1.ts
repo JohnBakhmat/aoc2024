28282c172caf31f76952d8fa784218ccdd22c762
John Bakhmat <johnbakhmat@gmail.com>
2024-12-02 07:01:38 +0300

    day 1 use deno std

--- 1.ts
+++ 1.ts
@@ -1,3 +1,6 @@
+import {} from "@std/collections";
+import { unzip } from "@std/collections";
+
 const real = true;
 const file = await Deno.readTextFile(`1.${real ? "real." : ""}txt`);
 const lines = file.trim().split("\n");
@@ -6,41 +9,21 @@ console.log(part1(lines));
 console.log(part2(lines));
 
 function part1(lines: string[]) {
-  const batch = lines.map((line) => {
-    return line.split("   ") as [string, string];
-  }).reduce((acc, cur) => {
-    acc[0].push(cur[0]);
-    acc[1].push(cur[1]);
-    return acc;
-  }, [[], []] as [Array<string>, Array<string>]);
-
-  const [left, right] = batch;
+  const data = lines.map((l) => l.split("   ") as [string, string]);
+  const [left, right] = unzip(data);
   left.sort();
   right.sort();
-
   const result = left.map((x, i) => Math.abs(+right[i] - +x));
   return result.reduce((a, c) => a + c, 0);
 }
+
 function part2(lines: string[]) {
-  const batch = lines.map((line) => {
-    return line.split("   ") as [string, string];
-  }).reduce((acc, cur) => {
-    acc[0].push(cur[0]);
-    acc[1].push(cur[1]);
-    return acc;
-  }, [[], []] as [Array<string>, Array<string>]);
-
-  const [left, right] = batch;
-  left.sort();
-  right.sort();
-
-  const freq = right.reduce((acc, cur) => {
-    const val = acc.get(cur);
-    acc.set(cur, (val ?? 0) + 1);
-    return acc;
-  }, new Map<string, number>());
-
+  const data = lines.map((l) => l.split("   ") as [string, string]);
+  const [left, right] = unzip(data);
+  const freq = right.reduce(
+    (a, c) => a.set(c, (a.get(c) ?? 0) + 1),
+    new Map<string, number>(),
+  );
   const result = left.map((x) => +x * (freq.get(x) ?? 0));
-
   return result.reduce((a, c) => a + c, 0);
 }
